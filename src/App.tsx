e96548fec5e252dc2a7b6abda73587dba61986e1
Periblue4haram <periblue4haram@gmail.com>
2026-08-15 00:40:22 +0900

    f5 화면 개발중

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Navigate, Routes, Route } from 'react-router-dom'
 import { DropStartPage } from '@/pages/DropStart'
 import { DesignRequirementPage } from '@/pages/DesignRequirement'
+import { MaterialCandidatesPage } from '@/pages/MaterialCandidates'
 import { MaterialRegistrationPage } from '@/pages/MaterialRegistration'
+import { ProductionScenarioPage } from '@/pages/ProductionScenario'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route path="/materials" element={<MaterialRegistrationPage />} />
       <Route path="/drops/new" element={<DropStartPage />} />
       <Route path="/drops/:dropId/design-requirement" element={<DesignRequirementPage />} />
+      <Route path="/drops/:dropId/candidates" element={<MaterialCandidatesPage />} />
+      <Route path="/drops/:dropId/production-scenario" element={<ProductionScenarioPage />} />
     </Routes>
   )
 }
